86087bf1e6d4799703ad27e18ac8d79eafdddbb4
snoor_madih <snoor.madih@gmail.com>
2021-06-11 20:49:32 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-
+import CatComponent from './CatComponent.js'
+import {GraceHopperQuoteComponent} from './GraceHopperQuoteComponent.js'
+import MouseComponent from './MouseComponent.js'
 class App extends Component {
 	render() {
 		// your code in the return statement below!
